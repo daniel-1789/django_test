dd6a3aa20114cc3b091008b8d622792445b06fa8
Daniel Stack <daniel@stackfamily.org>
2021-10-29 21:53:31 -0400

    first pass at defining model

--- transactions/models.py
+++ transactions/models.py
@@ -12,9 +12,39 @@ class FBATransaction(models.Model):
     class Meta:
         db_table = "fba_transactions"
 
-    # example_float = models.FloatField(default=0.0)
-    # example_date_time = models.DateTimeField(null=False, blank=False)
-    # example_foreign_key = models.ForeignKey("django_test.FBATransaction", null=True, blank=True, on_delete=models.SET_NULL)
-    # example_char = models.CharField(max_length=32, null=True, blank=True)
-    # example_int = models.IntegerField(null=True)
-    # example_decimal = models.DecimalField(null=True, max_digits=13, decimal_places=10)
+    date_time = models.DateTimeField(null=False, blank=False, db_column='date/time')  # never null
+
+    # order types
+    ADJUSTMENT = 'Adjustment'
+    RETURN_FEE = 'FBA Customer Return Fee'
+    INVENTORY_FEE = 'FBA Inventory Fee'
+    ORDER = 'Order'
+    ORDER_RETROCHARGE = 'Order_Retrocharge'
+    REFUND = 'Refund'
+    TRANSFER = 'Transfer'
+    ORDER_TYPE_CHOICES = [
+        (ADJUSTMENT, ADJUSTMENT),
+        (RETURN_FEE, RETURN_FEE),
+        (INVENTORY_FEE, INVENTORY_FEE),
+        (ORDER, ORDER),
+        (ORDER_RETROCHARGE, ORDER_RETROCHARGE),
+        (REFUND, REFUND),
+        (TRANSFER, TRANSFER),
+    ]
+    order_type = models.CharField(
+        max_length=32,
+        choices=ORDER_TYPE_CHOICES,
+        blank=True,
+        null=True,
+        db_column='type'
+    )
+
+    order_id = models.CharField(max_length=32, blank=True, null=True)
+    sku = models.CharField(max_length=32, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)  # never seen to be null
+    quantity = models.IntegerField(null=True)
+    order_city = models.CharField(max_length=64, blank=True, null=True)
+    order_state = models.CharField(max_length=32, blank=True, null=True)
+    order_postal = models.CharField(max_length=16, blank=True, null=True)
+    total = models.DecimalField(null=False, decimal_places=2, max_digits=16)
+
